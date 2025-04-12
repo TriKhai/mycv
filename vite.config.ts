fc14359d7be99bd52c777b80e82b8fa4aac41473
Tri Khai <trikhaikhmt@gmail.com>
2025-04-12 22:14:59 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mycv",
+  base: "/mycv/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
